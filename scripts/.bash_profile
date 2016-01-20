df3dc7d60d4dc025d8aaf2a8ea398c979e45c433
Zogg <zoggified@gmail.com>
2016-01-20 15:54:45 +0200

    Calico packaging

---
+++ scripts/.bash_profile
@@ -0,0 +1,4 @@
+export GOPATH=/home/vagrant/go
+export PATH=$PATH:$GOPATH/bin
+export GOPATH=/home/vagrant/go
+export PATH=$PATH:$GOPATH/bin
---
+++ scripts/bintray.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+BINTRAY_PROJECT='testing/calico'
+OUTPUT=${1:-out}
+
+for PKG in $(ls $OUTPUT); do
+    DOWNLOAD_URL="https://bintray.com/artifact/download/$BINTRAY_PROJECT/$PKG"
+    if curl -s -I $DOWNLOAD_URL | grep -q 302; then
+        echo "$PKG:	found, not uploading"
+        continue
+    fi
+
+    echo "$PKG:\tuploading"
+    URL=$(echo $PKG | sed -E "s|^(([a-zA-Z\-]+)-([0-9\.\-]+)\..*)$|https://api.bintray.com/content/$BINTRAY_PROJECT/\2/\3/\1|")
+    RESP=$(curl -T $OUTPUT/$PKG -u$BINTRAY_USER:$BINTRAY_API_KEY $URL?publish=1)
+    if [[ "$RESP" == '{"message":"success"}' ]]; then
+        echo "$PKG:	succesfully uploaded"
+    else
+        echo "$PKG:	error uploading"
+        echo $RESP
+        exit 1
+    fi
+done
